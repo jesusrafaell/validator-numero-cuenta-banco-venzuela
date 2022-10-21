ad56205b935028b3076a17614c4cad602823f1d9
Jesús R. Hernandez Vera <jhernandez@tranred.com.ve>
2022-10-21 17:05:54 -0400

    validador

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+function dc(numero: string, esCuenta: boolean): number {
+	let pesos = [];
+	if (esCuenta) pesos = [3, 2, 7, 6, 5, 4, 3, 2, 7, 6, 5, 4, 3, 2];
+	else pesos = [3, 2, 7, 6, 5, 4, 3, 2, 5, 4, 3, 2];
+
+	let s = 0;
+	for (let i = 0; i < numero.length; i++) {
+		let d = numero[i];
+		s += Number(d) * pesos[i];
+	}
+	let resultado = Number(11 - (s % 11));
+	if (resultado == 10) resultado = 0;
+	else if (resultado == 11) resultado = 1;
+
+	return resultado;
+}
+
+function getDigitoVerificador(entidad: string, sucursal: string, cuenta: string): string {
+	const num1 = dc(entidad + sucursal, false);
+	const num2 = dc(sucursal + cuenta, true);
+	const d = `${num1}${num2}`;
+	return d;
+}
+
+function isValid(account_number: string): boolean {
+	if (!/^[0-9]{20}$/.test(account_number)) return false;
+
+	const entidad: string = account_number.slice(0, 4);
+	const sucursal: string = account_number.slice(4, 8);
+	const dc: string = account_number.slice(8, 10);
+	const cuenta: string = account_number.slice(10);
+	const calculo = getDigitoVerificador(entidad, sucursal, cuenta);
+
+	if (calculo == dc) return true;
+
+	return false;
+}
+
+const test: string[] = [
+	'01340497634971022797', //si
+	'01020251580100014512', //si
+	'01340365163651030134', //no
+	'01020345300000670346', //no
+	'01340365163651030134', //no
+	'010201454500012218881', //no
+	'010209301500001360001', //no
+];
+
+test.forEach((st, index) => console.log(st, index + 1, isValid(st)));
